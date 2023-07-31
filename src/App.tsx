754e465685a6ccaa4fa14424bcdf88944f03c209
ruslan <r.karymov@vvdev.ru>
2023-07-31 18:42:44 +0300

    added Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'react-tabs/style/react-tabs.css';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <Tabs>
+                <TabList>
+                    <Tab>Dashboard</Tab>
+                    <Tab>Settings</Tab>
+                </TabList>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+            </Tabs>
+        </>
+    )
 }
 
 export default App
